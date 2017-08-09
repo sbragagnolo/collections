1b8e27c655832a6c033562449ba71cda694a653c
Santiago Bragagnolo <santiago.bragagnolo@inria.fr>
2017-08-09 19:14:51 +0200

    Cleaning up bullshit

--- CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/santiago/git-repositories/collections
-# Build directory: /home/santiago/git-repositories/collections
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(array)
--- template_subp/include/array/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/santiago/git-repositories/collections/array/include/array
-# Build directory: /home/santiago/git-repositories/collections/array/include/array
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
