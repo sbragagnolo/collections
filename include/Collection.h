2f34c4329e711f08e267d6a831e7f91254bfd30e
Santiago Bragagnolo <santiago.bragagnolo@inria.fr>
2017-08-22 17:57:22 +0200

    hardlinks

---
+++ include/Collection.h
@@ -0,0 +1,158 @@
+#ifndef COLLECTION_H_
+#define COLLECTION_H_
+
+#include<stdlib.h>
+#include<string.h>
+
+void setCallocFunction(void * (*)(size_t count, size_t size));
+void setMallocFunction(void * (*)(size_t count));
+void setReallocFunction(void * (*)(void* ptr, size_t size));
+void setFreeFunction(void (*)(void*ptr));
+
+void* (* getCallocFunction())(size_t count, size_t size);
+void* (* getMallocFunction())(size_t size);
+void* (* getReallocFunction())(void* ptr, size_t size);
+void (* getFreeFunction())(void* ptr);
+
+typedef enum {
+	TRUE = 1, FALSE = 0
+} boolean;
+
+typedef struct Collection {
+// ADD - REMOVE
+	int (*add)(struct Collection* self, void* item);
+	boolean (*remove)(struct Collection*, void*);
+	int (*addAll)(struct Collection* self, struct Collection otherCollection);
+// TEST
+	boolean (*allSatisfy)(struct Collection* self, boolean (*Condition)(struct Collection* self, void* item, void* aParameter), void* aParameter);
+	boolean (*anySatisfy)(struct Collection* self, boolean (*Condition)(void*self, void* item, void* aParameter), void* aParameter);
+// ITERATION
+	struct Collection* (*collect)(struct Collection* self, void* (*Map)(void* item));
+	void* (*detect)(struct Collection* self, boolean (*Comparator)(struct Collection* self, void* item, void*aParameter), void* aParameter);
+	void* (*inject)(struct Collection* self, void* (*Into)(struct Collection* self, void*item, void*Rslt), void* Rslt);
+	void* (*select)(struct Collection* self, boolean (*Condition)(void* item, void* aParameter), void* aParameter);
+	void (*forEach)(struct Collection* self, void (*function)(struct Collection* self, void* item, void* aParameter), void* aParameter);
+// COLLECTION 
+	int (*count)(struct Collection* self);
+	void* (*first)(struct Collection* self);
+	void* (*last)(struct Collection* self);
+	void* (*anyOne)(struct Collection* self);
+	void (*finalize)(struct Collection* self);
+
+} Collection;
+
+typedef struct ArrayedCollection {
+// ADD - REMOVE
+	int (*add)(struct ArrayedCollection* self, void* item);
+	boolean (*remove)(struct ArrayedCollection*, void*);
+	int (*addAll)(struct ArrayedCollection* self, struct Collection* otherCollection);
+// TEST
+	boolean (*allSatisfy)(struct ArrayedCollection* self, boolean (*Condition)(struct ArrayedCollection* self, void* item, void* aParameter), void* aParameter);
+	boolean (*anySatisfy)(struct ArrayedCollection* self, boolean (*Condition)(void*self, void* item, void* aParameter), void* aParameter);
+// ITERATION
+	struct Collection* (*collect)(struct ArrayedCollection* self, void* (*Map)(void* item));
+	void* (*detect)(struct ArrayedCollection* self, boolean (*Comparator)(struct ArrayedCollection* self, void* item, void*aParameter), void* aParameter);
+	void* (*inject)(struct ArrayedCollection* self, void* (*Into)(struct ArrayedCollection* self, void*item, void*Rslt), void* Rslt);
+	struct ArrayedCollection* (*select)(struct ArrayedCollection* self, boolean (*Condition)(void* item, void* aParameter), void* aParameter);
+	void (*forEach)(struct ArrayedCollection* self, void (*function)(struct ArrayedCollection* self, void* item, void* aParameter), void* aParameter);
+// COLLECTION 
+	int (*count)(struct ArrayedCollection* self);
+	void* (*first)(struct ArrayedCollection* self);
+	void* (*last)(struct ArrayedCollection* self);
+	void* (*anyOne)(struct ArrayedCollection* self);
+	void (*finalize)(struct ArrayedCollection* self);
+	void** data;
+	int size;
+	int used;
+} ArrayedCollection;
+
+int Arrayed_Collection_Add(struct ArrayedCollection* self, void* item);
+boolean Arrayed_Collection_Remove(struct ArrayedCollection*, void*);
+int Arrayed_Collection_AddAll(struct ArrayedCollection* self, struct Collection* otherCollection);
+
+boolean Arrayed_Collection_AllSatisfy(struct ArrayedCollection* self, boolean (*Condition)(struct ArrayedCollection* self, void* item, void* aParameter),
+		void* aParameter);
+boolean Arrayed_Collection_AnySatisfy(struct ArrayedCollection* self, boolean (*Condition)(void*self, void* item, void* aParameter), void* aParameter);
+
+Collection* Arrayed_Collection_Collect(struct ArrayedCollection* self, void* (*Map)(void* item));
+void* Arrayed_Collection_Detect(struct ArrayedCollection* self, boolean (*Comparator)(struct ArrayedCollection* self, void* item, void*aParameter),
+		void* aParameter);
+void* Arrayed_Collection_Inject(struct ArrayedCollection* self, void* (*Into)(struct ArrayedCollection* self, void*item, void*Rslt), void* Rslt);
+struct ArrayedCollection* Arrayed_Collection_Select(struct ArrayedCollection* self, boolean (*Condition)(void* item, void* aParameter), void* aParameter);
+void Arrayed_Collection_ForEach(struct ArrayedCollection* self, void (*function)(struct ArrayedCollection* self, void* item, void* aParameter),
+		void* aParameter);
+
+int Arrayed_Collection_Count(struct ArrayedCollection* self);
+void* Arrayed_Collection_First(struct ArrayedCollection* self);
+void* Arrayed_Collection_Last(struct ArrayedCollection* self);
+void* Arrayed_Collection_AnyOne(struct ArrayedCollection* self);
+void Arrayed_Collection_Finalize(struct ArrayedCollection* self);
+void Arrayed_Collection_Finalize_Object(struct ArrayedCollection* self);
+
+#define TYPED_ARRAYED_COLLECTION(Name,X) typedef struct Name {\
+	int (*add) (struct Name * self, X item); \
+	boolean(*remove) (struct Name *,X );\
+	int (*addAll) (struct Name * self, struct Collection* otherCollection);\
+	boolean(*allSatisfy) (struct Name * self, boolean(*Condition) (struct Name * self, X item, void* aParameter), void* aParameter);\
+	boolean(*anySatisfy) (struct Name * self, boolean(*Condition) (void*self, X item, void* aParameter), void* aParameter);\
+	Collection* (*collect) (struct Name * self, void* (*Map) (X item));\
+	X (*detect) (struct Name * self, boolean(*Comparator) (struct Name * self, X item, void*aParameter), void* aParameter);\
+	void* (*inject) (struct Name * self, void* (*Into) (struct Name * self, X item, void*Rslt), void* Rslt);\
+	struct Name * (*select) (struct Name * self, boolean(*Condition) (X item, void* aParameter), void* aParameter);\
+	void (*forEach) (struct Name * self, void (*function) (struct Name * self, X item, void* aParameter), void* aParameter);\
+	int (*count)(struct Name * self);\
+	X (*first) (struct Name * self);\
+	X (*last) (struct Name * self);\
+	X (*anyOne) (struct Name * self);\
+	void (*finalize) (struct Name *);\
+	X * data;\
+	int size;\
+	int used;\
+} Name;
+
+#define TYPED_ARRAYED_COLLECTION_INITIALIZE(Name,X) void Initialize_## Name ##_Object (struct Name * Instance){\
+        Instance->add = (int (*) (struct Name * self, X item)) Arrayed_Collection_Add;\
+	Instance->remove = (boolean(*) (struct Name *,X )) Arrayed_Collection_Remove;\
+	Instance->addAll = (int (*) (struct Name * self, struct Collection* otherCollection)) Arrayed_Collection_AddAll;\
+	Instance->allSatisfy = (boolean(*) (struct Name * self, boolean(*Condition) (struct Name * self, X item, void* aParameter), void* aParameter)) Arrayed_Collection_AllSatisfy;\
+	Instance->anySatisfy = (boolean(*) (struct Name * self, boolean(*Condition) (void*self, X item, void* aParameter), void* aParameter)) Arrayed_Collection_AnySatisfy;\
+	Instance->collect = (Collection* (*) (struct Name * self, void* (*Map) (X item))) Arrayed_Collection_Collect;\
+	Instance->detect = (X (*) (struct Name * self, boolean(*Comparator) (struct Name * self, X item, void*aParameter), void* aParameter)) Arrayed_Collection_Detect;\
+	Instance->inject = (void* (*) (struct Name * self, void* (*Into) (struct Name * self, X item, void*Rslt), void* Rslt)) Arrayed_Collection_Inject;\
+	Instance->select = (struct Name * (*) (struct Name * self, boolean(*Condition) (X item, void* aParameter), void* aParameter)) Arrayed_Collection_Select;\
+	Instance->forEach = (void (*) (struct Name * self, void (*function) (struct Name * self, X item, void* aParameter), void* aParameter)) Arrayed_Collection_ForEach;\
+	Instance->count = (int (*)(struct Name * self)) Arrayed_Collection_Count;\
+	Instance->first = (X (*) (struct Name * self)) Arrayed_Collection_First;\
+	Instance->last = (X  (*) (struct Name * self)) Arrayed_Collection_Last;\
+	Instance->anyOne = (X (*) (struct Name * self)) Arrayed_Collection_AnyOne;\
+	Instance->finalize = (void (*) (struct Name *)) Arrayed_Collection_Finalize;\
+	Instance->data = getCallocFunction()(5, sizeof(X));\
+	Instance->size = 5;\
+          Instance->used = 0; }
+
+
+
+
+
+
+
+#define TYPED_ARRAYED_COLLECTION_SIGNATURES(Name) \
+void Initialize_## Name ##_Object ( Name * Instance); \
+ Name * Name ##_New (void);
+
+#define TYPED_ARRAYED_COLLECTION_NEW(Name) struct Name * Name ##_New (){\
+  struct Name * ret = (struct Name *) getMallocFunction() (sizeof(struct Name));\
+  Initialize_## Name ##_Object(ret);\
+  ret->finalize = (void (*) (struct Name *))Arrayed_Collection_Finalize_Object;\
+  return ret;}
+
+#define TYPED_ARRAYED_COLLECTION_DEFINITION_H(Name,X)\
+  TYPED_ARRAYED_COLLECTION(Name,X);\
+  TYPED_ARRAYED_COLLECTION_SIGNATURES(Name);
+#define TYPED_ARRAYED_COLLECTION_DEFINITION_C(Name,X)\
+  TYPED_ARRAYED_COLLECTION_INITIALIZE(Name ,X);\
+  TYPED_ARRAYED_COLLECTION_NEW(Name);
+
+TYPED_ARRAYED_COLLECTION_SIGNATURES(ArrayedCollection);
+
+#endif
---
+++ test/collection_tests.c
@@ -0,0 +1,418 @@
+/*
+ * ModuleInstallationTests.cpp
+ *
+ *  Created on: Aug 1, 2017
+ *      Author: santiago
+ */
+
+#include <check.h>
+
+#include <Collection.h>
+
+int malloc_called = 0, realloc_called = 0, calloc_called = 0, free_called = 0;
+
+void * custom_malloc(size_t size) {
+	malloc_called++;
+	return (void*) malloc(size);
+}
+void * custom_calloc(size_t size, size_t t) {
+	calloc_called++;
+	return (void*) calloc(size, t);
+}
+void * custom_realloc(void* ptr, size_t size) {
+	realloc_called++;
+	return (void*) realloc(ptr, size);
+}
+void custom_free(void* ptr) {
+	free_called++;
+	free(ptr);
+}
+
+TYPED_ARRAYED_COLLECTION_DEFINITION_H(Generated, char*);
+TYPED_ARRAYED_COLLECTION_DEFINITION_C(Generated, char*);
+
+START_TEST(ArrayedCollectionMemoryLayoutAsExpected)
+	{
+		ck_assert_int_eq(sizeof(ArrayedCollection), (sizeof(void*) * 16) + (sizeof(int) * 2));
+	}END_TEST
+
+START_TEST(ArrayedCollectionCastsKeepsFunctionsPointersExpected)
+	{
+
+		ArrayedCollection* t;
+		Collection* c;
+		t = ArrayedCollection_New();
+		c = (Collection *) t;
+
+		ck_assert_ptr_eq(c->add, t->add);
+		ck_assert_ptr_eq(c->remove, t->remove);
+		ck_assert_ptr_eq(c->addAll, t->addAll);
+		ck_assert_ptr_eq(c->allSatisfy, t->allSatisfy);
+		ck_assert_ptr_eq(c->anySatisfy, t->anySatisfy);
+		ck_assert_ptr_eq(c->collect, t->collect);
+		ck_assert_ptr_eq(c->detect, t->detect);
+		ck_assert_ptr_eq(c->inject, t->inject);
+		ck_assert_ptr_eq(c->select, t->select);
+		ck_assert_ptr_eq(c->forEach, t->forEach);
+		ck_assert_ptr_eq(c->count, t->count);
+		ck_assert_ptr_eq(c->first, t->first);
+		ck_assert_ptr_eq(c->last, t->last);
+		ck_assert_ptr_eq(c->anyOne, t->anyOne);
+		ck_assert_ptr_eq(c->finalize, t->finalize);
+
+	}END_TEST
+
+START_TEST(ArrayedCollectionUsesArrayedCollectionFunctions)
+	{
+		ArrayedCollection c;
+		Initialize_ArrayedCollection_Object(&c);
+		ck_assert_ptr_eq(c.add, Arrayed_Collection_Add);
+		ck_assert_ptr_eq(c.remove, Arrayed_Collection_Remove);
+		ck_assert_ptr_eq(c.addAll, Arrayed_Collection_AddAll);
+		ck_assert_ptr_eq(c.allSatisfy, Arrayed_Collection_AllSatisfy);
+		ck_assert_ptr_eq(c.anySatisfy, Arrayed_Collection_AnySatisfy);
+		ck_assert_ptr_eq(c.collect, Arrayed_Collection_Collect);
+		ck_assert_ptr_eq(c.detect, Arrayed_Collection_Detect);
+		ck_assert_ptr_eq(c.inject, Arrayed_Collection_Inject);
+		ck_assert_ptr_eq(c.select, Arrayed_Collection_Select);
+		ck_assert_ptr_eq(c.forEach, Arrayed_Collection_ForEach);
+		ck_assert_ptr_eq(c.count, Arrayed_Collection_Count);
+		ck_assert_ptr_eq(c.first, Arrayed_Collection_First);
+		ck_assert_ptr_eq(c.last, Arrayed_Collection_Last);
+		ck_assert_ptr_eq(c.anyOne, Arrayed_Collection_AnyOne);
+		ck_assert_ptr_eq(c.finalize, Arrayed_Collection_Finalize);
+	}END_TEST
+
+START_TEST(ArrayedCollectionUsesSpecificFinalizeForNew)
+	{
+		ArrayedCollection * c = ArrayedCollection_New();
+		ck_assert_ptr_eq(c->add, Arrayed_Collection_Add);
+		ck_assert_ptr_eq(c->remove, Arrayed_Collection_Remove);
+		ck_assert_ptr_eq(c->addAll, Arrayed_Collection_AddAll);
+		ck_assert_ptr_eq(c->allSatisfy, Arrayed_Collection_AllSatisfy);
+		ck_assert_ptr_eq(c->anySatisfy, Arrayed_Collection_AnySatisfy);
+		ck_assert_ptr_eq(c->collect, Arrayed_Collection_Collect);
+		ck_assert_ptr_eq(c->detect, Arrayed_Collection_Detect);
+		ck_assert_ptr_eq(c->inject, Arrayed_Collection_Inject);
+		ck_assert_ptr_eq(c->select, Arrayed_Collection_Select);
+		ck_assert_ptr_eq(c->forEach, Arrayed_Collection_ForEach);
+		ck_assert_ptr_eq(c->count, Arrayed_Collection_Count);
+		ck_assert_ptr_eq(c->first, Arrayed_Collection_First);
+		ck_assert_ptr_eq(c->last, Arrayed_Collection_Last);
+		ck_assert_ptr_eq(c->anyOne, Arrayed_Collection_AnyOne);
+		ck_assert_ptr_eq(c->finalize, Arrayed_Collection_Finalize_Object);
+		c->finalize(c);
+	}END_TEST
+
+START_TEST(GeneratedCollectionUsesSpecificFinalizeForNew)
+	{
+		Generated * c = Generated_New();
+		ck_assert_ptr_eq(c->add, Arrayed_Collection_Add);
+		ck_assert_ptr_eq(c->remove, Arrayed_Collection_Remove);
+		ck_assert_ptr_eq(c->addAll, Arrayed_Collection_AddAll);
+		ck_assert_ptr_eq(c->allSatisfy, Arrayed_Collection_AllSatisfy);
+		ck_assert_ptr_eq(c->anySatisfy, Arrayed_Collection_AnySatisfy);
+		ck_assert_ptr_eq(c->collect, Arrayed_Collection_Collect);
+		ck_assert_ptr_eq(c->detect, Arrayed_Collection_Detect);
+		ck_assert_ptr_eq(c->inject, Arrayed_Collection_Inject);
+		ck_assert_ptr_eq(c->select, Arrayed_Collection_Select);
+		ck_assert_ptr_eq(c->forEach, Arrayed_Collection_ForEach);
+		ck_assert_ptr_eq(c->count, Arrayed_Collection_Count);
+		ck_assert_ptr_eq(c->first, Arrayed_Collection_First);
+		ck_assert_ptr_eq(c->last, Arrayed_Collection_Last);
+		ck_assert_ptr_eq(c->anyOne, Arrayed_Collection_AnyOne);
+		ck_assert_ptr_eq(c->finalize, Arrayed_Collection_Finalize_Object);
+		c->finalize(c);
+	}END_TEST
+START_TEST(GeneratedCollectionCastsKeepsFunctionsPointersExpected)
+	{
+
+		Generated* t = Generated_New();
+		Collection* c;
+		c = (Collection*) t;
+		ck_assert_ptr_eq(c->add, t->add);
+		ck_assert_ptr_eq(c->remove, t->remove);
+		ck_assert_ptr_eq(c->addAll, t->addAll);
+		ck_assert_ptr_eq(c->allSatisfy, t->allSatisfy);
+		ck_assert_ptr_eq(c->anySatisfy, t->anySatisfy);
+		ck_assert_ptr_eq(c->collect, t->collect);
+		ck_assert_ptr_eq(c->detect, t->detect);
+		ck_assert_ptr_eq(c->inject, t->inject);
+		ck_assert_ptr_eq(c->select, t->select);
+		ck_assert_ptr_eq(c->forEach, t->forEach);
+		ck_assert_ptr_eq(c->count, t->count);
+		ck_assert_ptr_eq(c->first, t->first);
+		ck_assert_ptr_eq(c->last, t->last);
+		ck_assert_ptr_eq(c->anyOne, t->anyOne);
+		ck_assert_ptr_eq(c->finalize, t->finalize);
+		t->finalize(t);
+	}END_TEST
+START_TEST(GeneratedCollectionUsesArrayedCollectionFunctions)
+	{
+
+		Generated c;
+		Initialize_Generated_Object(&c);
+		ck_assert_ptr_eq(c.add, Arrayed_Collection_Add);
+		ck_assert_ptr_eq(c.remove, Arrayed_Collection_Remove);
+		ck_assert_ptr_eq(c.addAll, Arrayed_Collection_AddAll);
+		ck_assert_ptr_eq(c.allSatisfy, Arrayed_Collection_AllSatisfy);
+		ck_assert_ptr_eq(c.anySatisfy, Arrayed_Collection_AnySatisfy);
+		ck_assert_ptr_eq(c.collect, Arrayed_Collection_Collect);
+		ck_assert_ptr_eq(c.detect, Arrayed_Collection_Detect);
+		ck_assert_ptr_eq(c.inject, Arrayed_Collection_Inject);
+		ck_assert_ptr_eq(c.select, Arrayed_Collection_Select);
+		ck_assert_ptr_eq(c.forEach, Arrayed_Collection_ForEach);
+		ck_assert_ptr_eq(c.count, Arrayed_Collection_Count);
+		ck_assert_ptr_eq(c.first, Arrayed_Collection_First);
+		ck_assert_ptr_eq(c.last, Arrayed_Collection_Last);
+		ck_assert_ptr_eq(c.anyOne, Arrayed_Collection_AnyOne);
+		ck_assert_ptr_eq(c.finalize, Arrayed_Collection_Finalize);
+	}END_TEST
+
+START_TEST(ArrayedCollectionCallsMacroMalloc)
+	{
+		ArrayedCollection c, *o;
+		ck_assert(malloc_called == 0);
+		Initialize_ArrayedCollection_Object(&c);
+		ck_assert(malloc_called == 0);
+		o = ArrayedCollection_New();
+		ck_assert(malloc_called == 1);
+		c.finalize(&c);
+		ck_assert(malloc_called == 1);
+		o->finalize(o);
+		ck_assert(malloc_called == 1);
+
+	}END_TEST
+START_TEST(ArrayedCollectionCallsMacroCalloc)
+	{
+		ArrayedCollection c, *o;
+		ck_assert(calloc_called == 0);
+		Initialize_ArrayedCollection_Object(&c);
+		ck_assert(calloc_called == 1);
+		o = ArrayedCollection_New();
+		ck_assert(calloc_called == 2);
+		c.finalize(&c);
+		ck_assert(calloc_called == 2);
+		o->finalize(o);
+		ck_assert(calloc_called == 2);
+	}END_TEST
+
+START_TEST(ArrayedCollectionCallsMacroFree)
+	{
+		ArrayedCollection c, *o;
+		ck_assert(free_called == 0);
+		Initialize_ArrayedCollection_Object(&c);
+		ck_assert(free_called == 0);
+		o = ArrayedCollection_New();
+		ck_assert(free_called == 0);
+		c.finalize(&c);
+		ck_assert(free_called == 1);
+		o->finalize(o);
+		ck_assert(free_called == 3);
+	}END_TEST
+START_TEST(ArrayedCollectionCallsMacroRealloc)
+	{
+		ArrayedCollection c;
+		int * data = malloc(sizeof(int));
+		*data = 1;
+
+		ck_assert(realloc_called == 0);
+
+		Initialize_ArrayedCollection_Object(&c);
+
+		c.add(&c, data);
+		ck_assert(realloc_called == 0);
+		c.add(&c, data);
+		ck_assert(realloc_called == 0);
+		c.add(&c, data);
+		ck_assert(realloc_called == 0);
+		c.add(&c, data);
+		ck_assert(realloc_called == 0);
+		c.add(&c, data);
+		ck_assert(realloc_called == 0);
+		c.add(&c, data);
+		ck_assert(realloc_called == 1);
+		c.add(&c, data);
+		ck_assert(realloc_called == 1);
+		c.add(&c, data);
+		ck_assert(realloc_called == 2);
+
+		c.finalize(&c);
+		ck_assert(realloc_called == 2);
+
+	}END_TEST
+
+int i;
+void ForEachTest(ArrayedCollection* self, void* item, void* aParameter) {
+	ck_assert(*(int* )item == 1 || *(int* )item == 2);
+	i++;
+}
+
+boolean ConditionEquals(void* item, void* aParameter) {
+
+	return (*((int*) item) == *((int*)aParameter)) ? TRUE : FALSE;
+
+}
+ArrayedCollection createTestedCollection() {
+	ArrayedCollection c;
+
+	int * data = malloc(sizeof(int));
+	int * data2 = malloc(sizeof(int));
+	*data = 1;
+	*data2 = 2;
+	Initialize_ArrayedCollection_Object(&c);
+
+	c.add(&c, data);
+	c.add(&c, data2);
+	c.add(&c, data);
+	c.add(&c, data2);
+	return c;
+
+}
+START_TEST(ArrayedCollectionForEachWorksAsExpected)
+	{
+		ArrayedCollection c = createTestedCollection();
+		i = 0;
+		c.forEach(&c, ForEachTest, NULL);
+		ck_assert_int_eq(i, 4);
+		c.finalize(&c);
+	}END_TEST
+
+void* IntoTest(struct ArrayedCollection* self, void*item, void*Rslt) {
+
+	int * param = (int*) Rslt;
+	int * each = (int*) item;
+	*param = *param + *each;
+
+	return Rslt;
+}
+
+START_TEST(ArrayedCollectionInjectIntoWorksAsExpected)
+	{
+		ArrayedCollection c = createTestedCollection();
+		int* value;
+		i = 0;
+		value = (int*) c.inject(&c, IntoTest, (void*) &i);
+		ck_assert_int_eq(*value, 6);
+		c.finalize(&c);
+	}END_TEST
+
+START_TEST(ArrayedCollectionSelectWorksAsExpected)
+	{
+		ArrayedCollection c = createTestedCollection();
+		i = 0;
+		ArrayedCollection * selection = c.select(&c, ConditionEquals, (void*) &i);
+		ck_assert_int_eq(selection->count(selection), 0);
+		selection->finalize(selection);
+
+		i = 1;
+		selection = c.select(&c, ConditionEquals, (void*) &i);
+		ck_assert_int_eq(selection->count(selection), 2);
+		selection->finalize(selection);
+
+		i = 2;
+		selection = c.select(&c, ConditionEquals, (void*) &i);
+		ck_assert_int_eq(selection->count(selection), 2);
+		selection->finalize(selection);
+
+		c.finalize(&c);
+
+	}END_TEST
+
+START_TEST(ArrayedCollectionRemoveWorksAsExpected)
+	{
+		ck_assert(1);
+	}END_TEST
+
+START_TEST(ArrayedCollectionAllSatisfyTrueWorksAsExpected)
+	{
+		ck_assert(1);
+	}END_TEST
+
+START_TEST(ArrayedCollectionAllSatisfyFalseWorksAsExpected)
+	{
+		ck_assert(1);
+	}END_TEST
+START_TEST(ArrayedCollectionAnySatisfyTrueWorksAsExpected)
+	{
+		ck_assert(1);
+	}END_TEST
+
+START_TEST(ArrayedCollectionAnySatisfyFalseWorksAsExpected)
+	{
+		ck_assert(1);
+	}END_TEST
+
+static void SetUp() {
+	malloc_called = 0;
+	calloc_called = 0;
+	realloc_called = 0;
+	free_called = 0;
+	setCallocFunction(custom_calloc);
+	setMallocFunction(custom_malloc);
+	setReallocFunction(custom_realloc);
+	setFreeFunction(custom_free);
+
+}
+static void TearDown() {
+
+}
+
+Suite * CreateSuite(void) {
+	Suite *s;
+	TCase *tc_structure;
+	TCase *tc_behaviour;
+
+	s = suite_create("Collection");
+
+	/* Core test case */
+	tc_structure = tcase_create("collection-structure");
+	tc_behaviour = tcase_create("collection-behaviour");
+	tcase_add_test(tc_structure, ArrayedCollectionMemoryLayoutAsExpected);
+	tcase_add_test(tc_structure, ArrayedCollectionCastsKeepsFunctionsPointersExpected);
+	tcase_add_test(tc_structure, ArrayedCollectionUsesArrayedCollectionFunctions);
+	tcase_add_test(tc_structure, ArrayedCollectionUsesSpecificFinalizeForNew);
+	tcase_add_test(tc_structure, GeneratedCollectionUsesSpecificFinalizeForNew);
+	tcase_add_test(tc_structure, GeneratedCollectionCastsKeepsFunctionsPointersExpected);
+	tcase_add_test(tc_structure, GeneratedCollectionUsesArrayedCollectionFunctions);
+
+	tcase_add_test(tc_behaviour, ArrayedCollectionCallsMacroMalloc);
+	tcase_add_test(tc_behaviour, ArrayedCollectionCallsMacroCalloc);
+
+	tcase_add_test(tc_behaviour, ArrayedCollectionCallsMacroFree);
+	tcase_add_test(tc_behaviour, ArrayedCollectionCallsMacroRealloc);
+	tcase_add_test(tc_behaviour, ArrayedCollectionForEachWorksAsExpected);
+	tcase_add_test(tc_behaviour, ArrayedCollectionInjectIntoWorksAsExpected);
+	tcase_add_test(tc_behaviour, ArrayedCollectionSelectWorksAsExpected);
+	tcase_add_test(tc_behaviour, ArrayedCollectionRemoveWorksAsExpected);
+	tcase_add_test(tc_behaviour, ArrayedCollectionAllSatisfyTrueWorksAsExpected);
+	tcase_add_test(tc_behaviour, ArrayedCollectionAllSatisfyFalseWorksAsExpected);
+	tcase_add_test(tc_behaviour, ArrayedCollectionAnySatisfyTrueWorksAsExpected);
+	tcase_add_test(tc_behaviour, ArrayedCollectionAnySatisfyFalseWorksAsExpected);
+
+	tcase_add_unchecked_fixture(tc_structure, SetUp, TearDown);
+	tcase_add_unchecked_fixture(tc_behaviour, SetUp, TearDown);
+
+	suite_add_tcase(s, tc_structure);
+	suite_add_tcase(s, tc_behaviour);
+
+	return s;
+}
+
+int main(void) {
+
+	int number_failed;
+	Suite *s;
+	SRunner *sr;
+
+	s = CreateSuite();
+	sr = srunner_create(s);
+
+	srunner_run_all(sr, CK_NORMAL);
+
+	number_failed = srunner_ntests_failed(sr);
+	srunner_free(sr);
+	return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+
+}
+
